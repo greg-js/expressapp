3be159a5733f85838ee9618f4355dab57d921850
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2014-11-27 17:23:01 +0000

    added routes, updated layout, fixed error in bootstrap

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,6 +3,10 @@
 // it takes the name of the view template and a JS data object
 // the .jade suffix is not necessary and the path to the view folder is declared in the main express setup
 
-exports.index = function(req, res) {
-  res.render('index.jade', { title: 'Express' });
+module.exports.about = function(req, res) {
+  res.render('index', { title: 'About' });
+};
+
+module.exports.signin = function(req, res) {
+  res.render('index', { title: 'Sign In' });
 };
--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var ctrl = require('../app_server/controllers/main.js');
 
 module.exports = function(app) {
-  app.get('/', ctrl.index);
+  app.get('/about', ctrl.about);
+  app.get('/signin', ctrl.signin);
 };
