f54fc0285a694902e9b49c0513126e2e6892ba44
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2014-12-17 18:55:03 +0000

    completed dynamic generation with jade

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -4,7 +4,10 @@
 // the .jade suffix is not necessary and the path to the view folder is declared in the main express setup
 
 module.exports.about = function(req, res) {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', { 
+    title: 'About',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nDolor cum voluptatem velit itaque ab dolorum quam? Perferendis architecto molestiae aliquam vitae maiores eveniet quo reprehenderit temporibus tenetur. Quaerat!\n\nLorem iure asperiores vel ab repellendus, ab, voluptatibus voluptatum voluptas quasi? Quo cum ullam aut eum reprehenderit maxime. Neque vitae expedita perspiciatis dolores et? Voluptatem iure commodi magni quas laborum?'
+  });
 };
 
 module.exports.signin = function(req, res) {
